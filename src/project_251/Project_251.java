7c58d742ece57ae5ffb6c8dddc2967624c528294
saraSultan2003 <143970178+saraSultan2003@users.noreply.github.com>
2023-09-10 19:47:15 +0300

    Update Project_251.java

--- src/project_251/Project_251.java
+++ src/project_251/Project_251.java
@@ -7,7 +7,7 @@ public class Project_251 {
     public static void main(String[] args) {
         System.out.println("Maather alrezqi");
        System.out.println("NADA ALSULAMI");
-       
+   System.out.println("sara sultan alharbi");
     
     }
     
