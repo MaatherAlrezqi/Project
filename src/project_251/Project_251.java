54db1ebb8678e284dd39d77b7ab7f91acb20ae55
MaatherAlrezqi <144177334+MaatherAlrezqi@users.noreply.github.com>
2023-09-10 20:48:15 +0300

    Update Project_251.java

--- src/project_251/Project_251.java
+++ src/project_251/Project_251.java
@@ -5,7 +5,7 @@ public class Project_251 {
 
    
     public static void main(String[] args) {
-        System.out.println("Maather alrezqi");
+        System.out.println("Maather Alrezqi");
        System.out.println("NADA ALSULAMI");
    System.out.println("sara sultan alharbi");
     
