7b9bf750e15ccfcbf2e4f99b0fd9efa4c0fde360
Nada Alsulami <119308324+NadaAbdr@users.noreply.github.com>
2023-09-10 19:33:35 +0300

    Update Project_251.java

--- src/project_251/Project_251.java
+++ src/project_251/Project_251.java
@@ -6,6 +6,8 @@ public class Project_251 {
    
     public static void main(String[] args) {
         System.out.println("Maather alrezqi");
+       System.out.println("NADA ALSULAMI");
+       
     
     }
     
