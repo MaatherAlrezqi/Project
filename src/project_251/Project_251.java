acc0a82d99579dad36e2f4c150df3a0b9e60ce48
sara_alharbi <sususu965@gmail.com>
2023-09-12 22:42:02 +0300

    sara sultan

--- src/project_251/Project_251.java
+++ src/project_251/Project_251.java
@@ -7,8 +7,8 @@ public class Project_251 {
     public static void main(String[] args) {
        System.out.println("Maather Alrezqi");
        System.out.println("NADA ALSULAMI");
-       System.out.println("sara sultan alharbi");
        System.out.println("Norah Ali ALqahtani");
+       System.out.println("sara sultan alharbi");
     
     }
     
