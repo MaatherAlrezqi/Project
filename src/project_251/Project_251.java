bbb6005e8eddb003e23e87d0f36f5693d30a6cbd
NADA ALSULAMI <nalialsulami@stu.kau.edu.sa>
2023-10-28 20:31:41 +0300

    github nada

--- src/project_251/Project_251.java
+++ src/project_251/Project_251.java
@@ -6,7 +6,7 @@ public class Project_251 {
    
     public static void main(String[] args) {
        System.out.println("Maather Alrezqi");
-       System.out.println("NADA ALSULAMI");
+       System.out.println("NADA ALSULAMI NEWWW");
        System.out.println("Norah Ali ALqahtani");
        System.out.println("sara sultan alharbi");
     
